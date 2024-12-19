a45db0fdf45955dea7ebf29fcabdc983fc3149dd
Carlos Farro <cfarro09>
2024-12-19 11:51:47 -0500

    f

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "strapi",
+        script: "npm",
+        args: "start",
+        env: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
